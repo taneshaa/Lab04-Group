6759e87be4a16dff0f0756ba49263d8d5b325096
Tanesha Brester <taneshabrester@gmail.com>
2022-01-26 17:25:07 -0800

    lab 06 complete

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("hello!");
+
+
+let petsName = prompt("what is your pets name?");
+console.log(petsName)
+alert(petsName)
+
+let zodiac = prompt("whats your zodiac sign?");
+console.log(zodiac)
+alert(zodiac)
+
+alert (petsName + zodiac);
