05bfe2b82205572883122b0ccca816045cfc9d0b
Tanesha Brester <taneshabrester@gmail.com>
2022-01-27 18:39:55 -0800

    lab 07

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ console.log(zodiac)
 alert(zodiac)
 
 alert (petsName + zodiac);
+
+function colorguessing(){
+    let color = prompt ("whats my favorite color?");
+    console.log (color)
+    alert(color)
+    if (color == 'green'){
+        alert ("You're right");
+    } else {
+        alert ("wrong answer");
+        colorguessing();}
+}
+
+colorguessing();
+
+//DECLARING MY DANG FUNCTION
+function petType () {
+    //ask user what type of pet do you have?
+let petType = prompt ('what type of pet do you have?');
+alert
+}
+
+// pass data onto functions?
